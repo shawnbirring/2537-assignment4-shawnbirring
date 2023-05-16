09f189aa5faed69a8f7d880c9a3ca448573f2f96
shawnbirring <shawnbirring21@gmail.com>
2023-05-16 16:06:02 -0700

    cool aditya

--- Interfaces/ICardState.ts
+++
@@ -1,6 +0,0 @@
-import Pokemon from "./IPokemon";
-export default interface CardState {
-  pokemon: Pokemon;
-  flipped: boolean;
-  canFlip: boolean;
-}
--- Interfaces/IPokemon.ts
+++
@@ -1,4 +0,0 @@
-export default interface Pokemon {
-  id: number;
-  image: string;
-}
